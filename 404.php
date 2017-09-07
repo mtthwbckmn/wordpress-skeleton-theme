da80ced7a76ccbc7ac59e5543927518e396cd710
Matthew Beckman <mtthwbckmn@gmail.com>
2017-09-06 22:15:54 -0700

    Added 404 error page.

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+	<section id="error">
+		<h1>Page Not Found</h1>
+		<p>It looks like what you're looking for isn't here.</p>
+		<div id="search">
+			<?php get_search_form(); ?>
+		</div>
+	</section>
+<?php get_footer(); ?>
