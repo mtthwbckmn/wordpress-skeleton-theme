5205c038f44bb6f3a2e80960a525111bd9812a25
Matthew Beckman <mtthwbckmn@gmail.com>
2017-09-17 11:32:43 -0700

    Fixed action.

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 /* Remove WordPress Backend Version Number */
 function removeVersion() {
-	remove_filter( 'update_footer', 'removeVersion' ); 
+	remove_filter( 'update_footer', 'core_update_footer' );
 }
 add_action( 'admin_menu', 'removeVersion' );
 
